986edd2763b035e520f8efc45f9b0ae16384b9f3
diop <fodesdiop@gmail.com>
2019-03-12 15:28:08 -0700

    add utils

--- bankcoin.py
+++ bankcoin.py
@@ -1,11 +1,6 @@
 from ecdsa import SigningKey, SECP256k1
 from utils import serialize
 
-
-bank_private_key = SigningKey.generate(curve=SECP256k1)
-bank_public_key = bank_private_key.get_verifying_key()
-
-
 def transfer_message(previous_signature, public_key):
     return serialize({
         "previous_signature": previous_ssignature,
@@ -20,17 +15,12 @@ class Transfer:
         self.public_key = public_key
 
 
-class ECDSACoin:
+class BankCoin:
     
     def __init__(self, transfers):
         self.transfers = transfers
 
     def validate(self):
-        # Check the first transfer
-        transfer = self.transfers[0]
-        message = serialize(transfer.public_key)
-        assert bank_public_key.verify(transfer.signature, message)
-
         # Check the subsequent transfers
         previous_transfer = self.transfers[0]
         for transfer in self.transfers[1:]:
@@ -44,16 +34,17 @@ class ECDSACoin:
 
 
 def issue(public_key):
-    # Create a message specifying who the coin is being issued to
-    message = serialize(public_key)
-    
-    # Create the first transfer, signing with the banks private key
-    signature = bank_private_key.sign(message)
     transfer = Transfer(
-        signature=signature,
+        signature=None,
         public_key=public_key,
     )
     
     # Create and return the coin with just the issuing transfer
     coin = ECDSACoin(transfers=[transfer])
     return coin
+
+class Bank:
+    def __init__(self):
+        # coin.id -> coin
+        self.coins = {}
+        
